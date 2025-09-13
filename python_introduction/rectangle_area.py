71f7368a30a11425ed3dfd6ded9f8324b579c89d
Emmanuel Billa <immanuelbilla33@gmail.com>
2025-09-13 19:24:54 +0000

    Corrected errors in rectangle_area.py and simple_interest.py

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,6 +1,6 @@
 length = 10
 width = 5
 
-Area = length * width
+area = length * width
 
-print("The area of the rectangle is:" + str(Area))
+print("The area of the rectangle is: " + str(area))
--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,6 +3,6 @@ rate = 0.05
 time = 3
 interest = 0
 
-interest = (principal*rate)/time
+interest = principal*rate*time
 
-print("The simple interest is:" + str(interest))
+print("The simple interest is: " + str(interest))
