cb99be82d52275992f70cac5e59763617031d540
Emmanuel Billa <immanuelbilla33@gmail.com>
2025-09-22 20:22:06 +0000

    2nd edit input prompt statements for daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
-task = input("Enter your task: ").lower()
+task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ").lower()
-time_bound = input("Is it time-bound? (yes or no): ").lower
+time_bound = input("Is it time-bound? (yes or no): ").lower()
 
 match priority:
     case "high":
--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,4 +1,4 @@
 hours = 2
 seconds = 3600 * hours
 
-print(str(hours) + " hour(s) is " + str(seconds) + " seconds")
+print(str(hours) + " hour(s) is " + str(seconds) + " seconds.")
