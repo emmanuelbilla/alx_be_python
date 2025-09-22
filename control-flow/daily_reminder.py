9329a864f33fca6da7bd8d5354682bfd1027defb
Emmanuel Billa <immanuelbilla33@gmail.com>
2025-09-22 20:27:15 +0000

    Removed conversion of inputs to lower

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
 task = input("Enter your task: ")
-priority = input("Priority (high/medium/low): ").lower()
-time_bound = input("Is it time-bound? (yes or no): ").lower()
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes or no): ")
 
 match priority:
     case "high":
