967bd9c654edf36f0d086eab7475f52ff65ac91a
Emmanuel Billa <immanuelbilla33@gmail.com>
2025-09-22 20:43:51 +0000

    Added 'Reminder' and 'Note' to ouput statements

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,18 +5,18 @@ time_bound = input("Is it time-bound? (yes/no): ")
 match priority:
     case "high":
         if time_bound == "yes":
-            print(task + " is a high priority task that requires immediate attention today!")
+            print("Reminder: " task + " is a high priority task that requires immediate attention today!")
         else:
-            print(task + " is a high priority task. Finish it today!")
+            print("Reminder: " task + " is a high priority task. Finish it today!")
     case  "medium":
         if time_bound == "yes":
-            print(task + " is a medium priority task. Get it done after today's high priority tasks!")
+            print("Reminder: " task + " is a medium priority task. Get it done after today's high priority tasks!")
         else:
-            print(task + " is a medium priority task. Find time for it!")
+            print("Note: " task + " is a medium priority task. Find time for it!")
     case "low":
         if time_bound == "yes":
-            print(task + " is a low priority task but requires immediate attention today!")
+            print("Reminder: " task + " is a low priority task but requires immediate attention today!")
         else:
-            print(task + " is a low priority task. Consider completing it when you have free time")
+            print("Note: " task + " is a low priority task. Consider completing it when you have free time")
     case _:
         print("You entered one or more wrong values. Recheck them and try again!")
